50af3bfeb2183f16d52c9323f4b13cfbb5222d32
songejin <96111650+songejin@users.noreply.github.com>
2023-05-19 16:41:43 +0900

    Add files via upload

---
+++ js/innerHTML.js
@@ -0,0 +1,5 @@
+document.getElementById('header').innerHTML = '<div id="logo"><h1><a href="index.html"><img src="img/logo.png" alt="서울컬쳐 메인 로고"></a></h1></div><nav id="lnb"><h3 class="hidden">메뉴</h3><ul><li><a href="scenery.html">소소서울</a></li><li><a href="humanities.html">인문학특강</a></li><li><a href="old_store.html">오래가게</a></li><li><a href="taste.html">취향의발견</a></li><li><a href="attraction.html">겨울명소</a></li></ul></nav>';
+
+document.getElementById('footer').innerHTML ='<div id="fnb"><address><small>발행인 서울특별시장 박원순 | 편집인 시민소통기획관 유연식 | 제작 부서 뉴미디어담당관 담당관 이종선 | 편집장 송영미 | 취재·편집 한해아, 나영완<br> 편집위원 김연휘(몽키랩 대표), 김인호(서울시의원), 박초롱(연합뉴스 기자), 서진수(야놀자 브랜드디자인 실장), 윤영호(산돌커뮤니케이션 대표)<br> 발행일 2019년 1월 1일 통권 제196호 | 발간등록번호 51-6110000-000442-06 | 발행처 서울특별시 | 디자인·인쇄 더북컴퍼니</small></address></div><div id="bottom"><h2><a href="index.html"><img src="img/logo_k.png" alt="서울컬쳐 하단 로고"></a></h2></div>'
+
+document.getElementById('top').innerHTML = '<a href="#header"><img src="img/top.png" alt="맨위로가기 버튼 이미지"></a>'
